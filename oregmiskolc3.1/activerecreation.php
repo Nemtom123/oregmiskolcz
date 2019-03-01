4c4dc7286707aae3d32635629b26896a8446f0a7
Nemtom123 <dobiasz.tamas@gmail.com>
2019-03-01 07:36:19 +0100

    Add files via upload

---
+++ oregmiskolc3.1/activerecreation.php
@@ -0,0 +1,33 @@
+<div id="service4" >
+	<section class="section-block grey">
+		<div class="container" style="background-color: #F3F3F3; width: 100%; height: 150px">
+			<main id="main">
+				<section class="section-block first-section-type no-spacing-bottom">
+					<div class="hr-sect" id="formazashead">
+						Öreg Miskolcz Hotel & Étterem
+					</div>
+					<div id="formazascenter">Aktív pihenés</div>
+				</section>
+			</main>
+		</div>
+	</section>
+	<div class="col-sm-6 col-lg-6" >
+		<div class="blogTxt" id="formaztext">
+			<div class="blogCategory">
+				<h2 id="formaztextbekezdes">Aktív pihenés ajánlatunk</h2>
+			</div>
+			<p id="formaztext"><h2>A férfiak többsége vörös rózsával kedveskedik párjának.</h2>
+			<p id="formaztext"> Az éttermek speciális menüsorokkal készültek az ünnepre: a szerelmes párok
+				már hetekkel </p>
+			<p id="formaztext">ezelőtt megkezdték
+				az asztalfoglalásokat két személyre. </p><p id="formaztext">A felnőttek mellett a gyermekek is készültek a szerelem napjára.</p>
+		</div>
+	</div>
+    <div class="col-sm-6 col-lg-6 gallery"  id="formazaskepjobb">
+        <figure>
+            <img src="images/active/1.jpg" class="img-responsive zoom">
+        </figure>
+    </div>
+
+</div>
+<div class="clearfix"></div>
